e9a9fae77e95a1a33df3cfef633323a3dfdbf162
sean-gay <50360222+sean-gay@users.noreply.github.com>
2020-07-14 11:45:23 -0400

    Update message.py

--- src/message.py
+++ src/message.py
@@ -6,9 +6,9 @@ import os
 if __name__ == "__main__":
     print("here")
     #grab the connector card first, then perform action
-    myTeamsMessage = pymsteams.connectorcard("https://outlook.office.com/webhook/f1440fea-6baa-450b-b733-72ea9e3bcd1b@99dd3a11-4348-4468-9bdd-e5072b1dc1e6/IncomingWebhook/f58e83d790c04bde8a88ca17e08a8ad3/cd3f2add-89d4-4d63-bc4c-c0eca66f4cc0")
+    myTeamsMessage = pymsteams.connectorcard("<URL>")
 
-    stream = os.popen(f"/Applications/MATLAB_R2017b.app/bin/matlab -nodesktop -nosplash -r 'cmds=\"{sys.argv[1]}\";disp(eval(cmds));exit;'")
+    stream = os.popen(f"<MATLAB PATH> -nodesktop -nosplash -r 'cmds=\"{sys.argv[1]}\";disp(eval(cmds));exit;'")
     output = stream.read()
 
     myTeamsMessage.text(output)
